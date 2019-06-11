ac8aa94fee8f657e8967fb290f0a0c2666ac69b8
Stephen MacVicar <smacvicar@mitre.org>
2019-06-11 11:17:03 -0400

    add end-to-end generation

---
+++ generate.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'pry'
+require_relative 'lib/formulary/config'
+require_relative 'lib/formulary/drug_list_generator'
+require_relative 'lib/formulary/qhp_drug_repo'
+require_relative 'lib/formulary/qhp_importer'
+require_relative 'lib/formulary/qhp_plan_repo'
+
+config = Formulary::Config
+drug_repo = Formulary::QHPDrugRepo
+plan_repo = Formulary::QHPPlanRepo
+
+Formulary::QHPImporter
+  .new(config.plan_urls, plan_repo)
+  .import
+Formulary::QHPImporter
+  .new(config.drug_urls, drug_repo)
+  .import
+
+plan_repo.all.each do |plan|
+  Formulary::DrugListGenerator.new(plan).generate
+end
