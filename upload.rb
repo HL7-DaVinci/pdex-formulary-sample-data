4a0c167286f3a70a83ad6676da345edd2200ac41
Stephen MacVicar <smacvicar@mitre.org>
2019-06-24 08:35:59 -0400

    upload formulary conformance resources from the published build instead of github

--- upload.rb
+++ upload.rb
@@ -1,12 +1,13 @@
 require 'pry'
 require 'git'
+require 'zip'
 require 'httparty'
 require 'tmpdir'
 require 'fileutils'
 
 FHIR_SERVER = 'http://localhost:8080/r4'
 
-def upload_conformance_resources
+def upload_conformance_resources_from_git
   begin
     file_path = File.join(Dir.tmpdir, SecureRandom.uuid)
     git_repo = Git.clone('https://github.com/HL7/davinci-pdex-formulary.git', file_path)
@@ -22,6 +23,30 @@ def upload_conformance_resources
   ensure
     FileUtils.rm_rf(file_path)
   end
+end
+
+def upload_conformance_resources
+  definitions_url = 'http://build.fhir.org/ig/HL7/davinci-pdex-formulary/definitions.json.zip'
+  definitions_data = HTTParty.get(definitions_url, verify: false) # FIXME
+  definitions_file = Tempfile.new
+  begin
+    definitions_file.write(definitions_data)
+  ensure
+    definitions_file.close
+  end
+
+  Zip::File.open(definitions_file.path) do |zip_file|
+    zip_file.entries
+      .select { |entry| entry.name.end_with? '.json' }
+      .reject { |entry| entry.name.start_with? 'ImplementationGuide' }
+      .each do |entry|
+        resource = JSON.parse(entry.get_input_stream.read, symbolize_names: true)
+        response = upload_resource(resource)
+        binding.pry unless response.success?
+      end
+  end
+ensure
+  definitions_file.unlink
 end
 
 def upload_sample_resources
