67d83911f07baae198af1560f37047448cfcc0e6
Benjamin Langley <blangley@mitre.org>
2020-12-21 11:24:39 -0500

    Clean up  file generation

--- convertNDJSON.rb
+++ convertNDJSON.rb
@@ -1,35 +1,37 @@
 require 'json'
 require 'pry'
-    indir = ARGV[0]   # point to output directory
+
+FHIR_SERVER_BASE = "http://davinci-plan-net-ri.logicahealth.org/plan-net"
+
 # First output the *List*.json, these are the CoveragePlan profile instances
-puts "working on input directory: #{indir}..."
+puts "working on input directory: output..."
+FileUtils.mkdir_p("export/")
 outfile = "CoveragePlan.ndjson"
 ndouts = []
 ndouts << {
     "type" => "List",
-    "url" => outfile
+    "url" => "#{FHIR_SERVER_BASE}/resources/#{outfile}"
 }
-puts "writing to #{outfile}"
-    o = File.open(outfile,"w")
-    Dir.glob("#{indir}/*.json") do |jsonfile|
+puts "writing to export/#{outfile}"
+    o = File.open("export/#{outfile}","w")
+    Dir.glob("output/*.json") do |jsonfile|
         puts "working on: #{jsonfile}..."
         s = File.read(jsonfile)
         h = JSON.parse(s)
         o.puts(JSON.generate(h))
     end
     o.close
-# Next iterate through the directories, each one contains the MedicationKnowledge profile instances for a single plan
-    Dir.glob("#{indir}/*/") do |plandir|
-        # puts "working on input directory: #{plandir}..."
+    # Next iterate through the directories, each one contains the MedicationKnowledge profile instances for a single plan
+    Dir.glob("output/*/") do |plandir|
         outfile = "FormularyDrug_#{File.basename(plandir)}.ndjson"
         puts "writing to #{outfile}"
+        url = "#{FHIR_SERVER_BASE}/resources/#{outfile}"
         ndouts << {
             "type" => "MedicationKnowledge",
-            "url" => outfile
+            "url" => url
         }
-        o = File.open(outfile,"w")
+        o = File.open("export/#{outfile}","w")
         Dir.glob("#{plandir}/*.json") do |jsonfile|
-            # puts "working on: #{jsonfile}..."
             s = File.read(jsonfile)
             h = JSON.parse(s)
             o.puts(JSON.generate(h))
@@ -38,11 +40,12 @@ puts "writing to #{outfile}"
     end
     output = {
         "transactionTime" => Time.now.strftime("%d/%m/%Y %H:%M"),
-        "request" => "<baseFhirURL>/$export",
+        "request" => "#{FHIR_SERVER_BASE}/fhir/$export",
         "requiresAccessToken" => false,
         "output" => ndouts,
         "error" => { "type" => "OperationOutcome",
-                    "url" =>  "http://serverpath2/err_file_1.ndjson"}
+                    "url" =>  "http://#{FHIR_SERVER_BASE}/resources/err_file_1.ndjson"}
     }
-    export = File.open("export.json","w")
+    export = File.open("export/export.json","w")
     export.write(JSON.pretty_generate(output))
+    puts "Files written to /export"
