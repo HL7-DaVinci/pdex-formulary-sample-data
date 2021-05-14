fce66271a7c4699ec97165dcd93f46a7e5c0f68d
Mike Ogren <mogren@devoted.com>
2021-05-14 14:32:56 -0400

    updating config

--- lib/formulary/qhp_importer.rb
+++ lib/formulary/qhp_importer.rb
@@ -14,14 +14,16 @@ module Formulary
 
     def import
       urls.each do |url|
-        if url.start_with?("http") then
-          qhp_raw_data = HTTParty.get(url, verify: false) # FIXME
-        else
-          qhp_raw_data = File.read(url)
+        file_path = File.join(url,  '**/*.json')
+        filenames = Dir.glob(file_path)
+        puts "Reading #{filenames.length} file(s) from #{url}"
+
+        filenames.each do |filename|
+          qhp_raw_data = File.read(filename)
+          qhp_json.concat(JSON.parse(qhp_raw_data, symbolize_names: true))
+          repo.import(qhp_json)
         end
-        qhp_json.concat(JSON.parse(qhp_raw_data, symbolize_names: true))
       end
-      repo.import(qhp_json)
     end
 
     def qhp_json
